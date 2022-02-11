f682610dc944874f9812dbed9e5203d4e3f01da6
Veerababu T <t.veerababu32@gmail.com>
2022-02-11 14:27:28 +0530

    changed some styles in home

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,20 @@ const browsersync=require('browser-sync').create();
 function style(){
   return gulp.src('./scss/style.scss')
   .pipe(sass())
-  .pipe(gulp.dest('./css/style.css'))
+  .pipe(gulp.dest('./css'))
   .pipe(browsersync.stream());
+}
 
-  }
+function watch(){
+    browsersync.init({
+                server:{
+                   baseDir:'./'
+                }
+    });
+    gulp.watch('./scss/**/*.scss',style);
+    gulp.watch('./*.html').on('change',browsersync.reload);
+    gulp.watch('./js/**/*.js').on('change',browsersync.reload);
+}
 
-
-  function watch(){
-     browsersync.init({
-                  server:{
-                     baseDir:'./'
-                       }
-      });
-      gulp.watch('./css/**/*.css',style);
-      gulp.watch('./*.html').on('change',browsersync.reload);
-      gulp.watch('./js/**/*.js').on('change',browsersync.reload);
-  }
-
-  exports.style=style;
-  exports.watch=watch;
+exports.style=style;
+exports.watch=watch;
